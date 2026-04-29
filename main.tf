05c1cb10608a55410106396305e2f4d888ca4a40
yaminiduraisingam-env <yamini.duraisingam@env0.com>
2026-04-29 10:12:12 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 # No-op OpenTofu configuration
 # This does nothing — useful for testing env0 version installs
 
+providers = {
+  required = "1.11.3"
+}
 output "hello" {
   value = "OpenTofu is working!"
 }
